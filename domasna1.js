bf2b86ab5ed193ad86cc71a11c31b6577fd8572a
Paskalija <paskalija@gmail.com>
2021-05-07 10:41:06 +0200

    prva_verzija

---
+++ domasna1.js
@@ -0,0 +1,44 @@
+let ime = 'Paskalija';
+let godini = 24;
+let isBurnette = true;
+let isBlonde = false;
+let prezime = null;
+
+console.log(ime);
+console.log(godini);
+console.log(isBurnette);
+console.log(isBlonde)
+console.log(prezime);
+
+console.log(typeof ime);
+console.log(typeof godini);
+console.log(typeof isBurnette);
+console.log(typeof isBlonde);
+console.log(typeof prezime);
+
+const fave = 357;
+
+
+let student = {
+    fName = 'Paki',
+    lName = 'Koneska',
+    age = 24
+}
+
+let student2 = {
+    fName = 'Kiril',
+    lName = 'Jordanov',
+    age = 23
+}
+
+
+let people = [student, student2];
+
+console.log(student);
+console.log(student2);
+console.log(people);
+
+console.log(typeof student);
+console.log(typeof student2);
+console.log(typeof people);
+
