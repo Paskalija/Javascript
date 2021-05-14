931858479455c3c7f2bc8852d4c30df5e717ba93
Paskalija <paskalija@gmail.com>
2021-05-14 16:19:52 +0200

    prva verzija

---
+++ domasna3.js
@@ -0,0 +1,38 @@
+//PRVA VEZBA
+let p = 3
+let k = 5
+let j = 7 
+
+p =+ 4;
+console.log('p =', p);
+
+k =- 2;
+console.log('k =', k);
+
+j =- 1;
+console.log('j =', j);
+
+
+
+//VTORA VEZBA
+let x = 333;
+let y = 555;
+let z = 777;
+
+if ( x > y && x > z) {
+
+    if ( y > z ){
+    console.log ( x, y, z);
+    }
+    else {
+        console.log( x, z, y );
+    }
+
+}
+
+//TRETA VEZBA
+
+let studenti = [ ('David', 80), ('Vinoth', 77), ('Divya', 88), ('Ishitha', 95), ('Thomas', 68)];
+
+let prosek = 0;
+
